277861ed45185cd14277b8ace662e1ec0006923e
Brooke McFarland <brookelyn.mcf@gmail.com>
2016-05-07 17:34:36 -0500

    initial commit

---
+++ program8.py
@@ -0,0 +1,19 @@
+########################################################################
+##
+## CS 101
+## Program # 6
+## Name Brooke McFarland
+## Email blmz99@mail.umkc.edu
+##
+## PROBLEM : Describe the problem
+##
+## ALGORITHM : 
+##      1. Write out the algorithm
+## 
+## ERROR HANDLING:
+##      Any Special Error handling to be noted.  Wager not less than 0. etc
+##
+## OTHER COMMENTS:
+##      Any special comments
+##
+########################################################################
