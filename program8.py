b80bca7f7608c36db3c403c56d277b81011c2eda
Brooke McFarland <brookelyn.mcf@gmail.com>
2016-05-07 18:23:48 -0500

    import random and creation of park class

--- program8.py
+++ program8.py
@@ -17,3 +17,20 @@
 ##      Any special comments
 ##
 ########################################################################
+import random
+
+class Park(object):
+    def __init__(self,lots = 2, tourist_num = 0 ):
+        self.lots = lots
+        self.tram = tram
+        self.tourist = tourist
+        self.lots_total = lots_total
+        self.tourist_num = tourist_num
+    def __str__(self):
+        lot = 0
+        while lot <= lots_total:
+            lot += 1
+        print("{}==".format(lot))
+    def step(self):
+        pass
+
